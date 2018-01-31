e6e6ca511ad381fd0b5b52c842cd662165538d80
OcDemian <Pixar.1994@gmail.com>
2018-01-31 17:59:47 +0400

    1 day

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Пух-Платок</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+
+      </div>
+      <div class="menu_background">
+        <div class="container menu_container">
+          <ul class="menu">
+
+          </ul>
+          <div class="basket_btn">
+
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <footer>
+      <div class="social_background">
+        <div class="container social">
+          <a class="fb" href="#"></a>
+          <a class="vk" href="#"></a>
+          <a class="ok" href="#"></a>
+          <a class="tw" href="#"></a>
+          <a class="you" href="#"></a>
+          <a class="pp" href="#"></a>
+          <a class="dm" href="#"></a>
+        </div>
+      </div>
+      <div class="footer_mid">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <div class="logo_footer">
+                <img src="/img/logo_footer.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-3 footer_catalog">
+              <a href="#">ДЛЯ <br />МУЖЧИН</a>
+              <a href="#">ДЛЯ <br />ЖЕНЩИН</a>
+              <a href="#">ДЛЯ <br />ДЕТЕЙ</a>
+              <a href="#"><b>РАСПРО<br />ДАЖА</b></a>
+            </div>
+            <div class="col-md-3">
+              <div class="footer_menu">
+                <ul class="footer_menu_ul">
+                  <li><a href="">О компании</a></li>
+                  <li><a href="">Доставка и оплата</a></li>
+                  <li><a href="">Помощь</a></li>
+                  <li><a href="">Контакты</a></li>
+                  <li><a href="">Личный кабинет</a></li>
+                  <li><a href="">Поиск по сайту</a></li>
+                  <li><a href="">Карта сайта</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="footer_phone">
+                Звонок по россии<br />
+                бесплатный<br />
+                <a href="88005556607">8 800 555 66 07</a><br /><br />
+                Москва<br />
+                <a href="84952150863">8 (495) 215-08-63</a><br /><br />
+                Санкт-Петербург<br />
+                <a href="84952150863">8 (495) 215-08-63</a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/slick/slick.min.js"></script>
+  </body>
+</html>
